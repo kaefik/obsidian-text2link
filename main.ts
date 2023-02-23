fb36d5e954f8ee5db2efd4c31d7ca3fc9948b54c
ilnursoft <ilnursoft@gmail.com>
2023-02-23 23:37:05 +0300

    fix доавления маркдаун ссылки е6сли текущая папка состоит из нескольких слов и также добавлена вставка вики ссылок

--- main.ts
+++ main.ts
@@ -46,23 +46,29 @@ export default class MyPlugin extends Plugin {
 
 				var cur_path = cur_file.path
 				var cur_filename = cur_file.name
-				// получаем текущую папку в vault
-				var cur_onlypath = cur_file.parent.path
-				// console.log("Current File: ")
-				// console.log(cur_onlypath)
 
 				var cur_text = editor.getSelection()
-				var cur_text_link = encodeURIComponent(cur_text.trim())
 
 				if (this.settings.wikiLinks) {
 					console.log("Create wiki links")
+					var cur_text_link = cur_text.trim()
+					var cur_onlypath = cur_file.parent.path
+					var subfolder_link = cur_onlypath + "/"+this.settings.subFolders.trim()
+					console.log("subfolder_link: ", subfolder_link)
+
+					cur_text = `[[${subfolder_link}/${cur_text_link}|${cur_text}]]`
+
 				} else {
-					console.log("Create Markdown links")
+					console.log("Create Markdown links")			
+					var cur_text_link = encodeURIComponent(cur_text.trim())				
+					var cur_onlypath = cur_file.parent.path.split(' ').join('%20');
+					console.log("Current File: ")
+					console.log(cur_onlypath)
 					var subfolder_link = cur_onlypath + "/"+encodeURIComponent(this.settings.subFolders.trim())
-					// console.log(cur_text);
-					cur_text = `[${cur_text}](${subfolder_link}/${cur_text_link})`
-					editor.replaceSelection(cur_text);
+					
+					cur_text = `[${cur_text}](${subfolder_link}/${cur_text_link})`					
 				}
+				editor.replaceSelection(cur_text);
 			}
 		});
 
@@ -84,8 +90,6 @@ export default class MyPlugin extends Plugin {
 				
 				var many_cur_text = editor.getSelection()
 				var arr_sur_text = many_cur_text.split('\n')
-				
-				var subfolder_link = cur_onlypath + "/"+encodeURIComponent(this.settings.subFolders.trim())
 
 				console.log(arr_sur_text)
 
@@ -93,52 +97,31 @@ export default class MyPlugin extends Plugin {
 					{ 
 						var cur_text = arr_sur_text[index].trim()
 						if (cur_text != '') {
-							var cur_text_link = encodeURIComponent(cur_text.trim())							
-							console.log(cur_text);
-							cur_text = `[${cur_text}](${subfolder_link}/${cur_text_link})\n`
+							if (this.settings.wikiLinks) {
+								console.log("Create wiki links")
+								var cur_text_link = cur_text.trim()
+								var cur_onlypath = cur_file.parent.path
+								var subfolder_link = cur_onlypath + "/"+this.settings.subFolders.trim()
+								console.log("subfolder_link: ", subfolder_link)
+			
+								cur_text = `[[${subfolder_link}/${cur_text_link}|${cur_text}]]\n`
+			
+							} else {
+								console.log("Create Markdown links")			
+								var cur_text_link = encodeURIComponent(cur_text.trim())				
+								var cur_onlypath = cur_file.parent.path.split(' ').join('%20');
+								console.log("Current File: ")
+								console.log(cur_onlypath)
+								var subfolder_link = cur_onlypath + "/"+encodeURIComponent(this.settings.subFolders.trim())
+								
+								cur_text = `[${cur_text}](${subfolder_link}/${cur_text_link})\n`					
+							}
 							editor.replaceSelection(cur_text);
 						}
 					}
 			}
 		});
-
-
-		// // This adds a simple command that can be triggered anywhere
-		// this.addCommand({
-		// 	id: 'open-sample-modal-simple',
-		// 	name: 'Open sample modal (simple)',
-		// 	callback: () => {
-		// 		new SampleModal(this.app).open();
-		// 	}
-		// });
-		// // This adds an editor command that can perform some operation on the current editor instance
-		// this.addCommand({
-		// 	id: 'sample-editor-command',
-		// 	name: 'Sample editor command',
-		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
-		// 		console.log(editor.getSelection());
-		// 		editor.replaceSelection('Sample Editor Command');
-		// 	}
-		// });
-		// // This adds a complex command that can check whether the current state of the app allows execution of the command
-		// this.addCommand({
-		// 	id: 'open-sample-modal-complex',
-		// 	name: 'Open sample modal (complex)',
-		// 	checkCallback: (checking: boolean) => {
-		// 		// Conditions to check
-		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 		if (markdownView) {
-		// 			// If checking is true, we're simply "checking" if the command can be run.
-		// 			// If checking is false, then we want to actually perform the operation.
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-
-		// 			// This command will only show up in Command Palette when the check function returns true
-		// 			return true;
-		// 		}
-		// 	}
-		// });
+		
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
